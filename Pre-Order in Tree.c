63b7d5bc294087cbb7dda58eb65f73e72e1c41be
Sahil-85-Sehrawat <sehrawatsahil444@gmail.com>
2025-05-07 09:27:38 +0530

    Create Pre-Order in Tree.c

---
+++ Pre-Order in Tree.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void preorder(struct Node* root) {
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    
+    printf("Preorder Traversal: ");
+    preorder(root);
+    printf("\n");
+    
+    return 0;
+}
